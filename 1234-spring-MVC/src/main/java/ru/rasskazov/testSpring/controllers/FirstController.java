ddaa152c0582002eba774e396e1f9aeb31f6c001
19300658 <DAnRasskazov@sberbank.ru>
2022-12-16 16:09:31 +0300

    Works with calculator. TODO - division by zero

--- 1234-spring-MVC/src/main/java/ru/rasskazov/testSpring/controllers/FirstController.java
+++ 1234-spring-MVC/src/main/java/ru/rasskazov/testSpring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package ru.rasskazov.testSpring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,9 +25,43 @@ public class FirstController {
 
     @GetMapping("/goodbye")
     public String goodbyePage(@RequestParam(value = "name", required = false) String name,
-                              @RequestParam(value = "surname", required = false) String surname)
+                              @RequestParam(value = "surname", required = false) String surname,
+                                Model model)
     {
+
+        model.addAttribute("message", "Hello, " + name + " " +  surname);
+
         System.out.println(name + " and " + surname);
         return "/first/goodbye";
+    }
+
+    @GetMapping("/calc")
+    public String calcPage(@RequestParam(value = "a", required = false) String a,
+                           @RequestParam(value = "b", required = false) String b,
+                           @RequestParam(value = "action", required = false) String action,
+                           Model model) {
+        int result;
+        String answer = null;
+
+        if (action.equals("divison")) {
+            if (Integer.parseInt(b) == 0) {
+                answer = "Division by zero"; //почему-то не работает при делении на ноль
+                System.out.println(answer);
+            } else {
+                result = Integer.parseInt(a) / Integer.parseInt(b);
+                answer = String.valueOf(result);
+            }
+        } else {
+            result = 0;
+            if (action.equals("addition"))
+                result = Integer.parseInt(a) + Integer.parseInt(b);
+            else if (action.equals("subtraction"))
+                result = Integer.parseInt(a) - Integer.parseInt(b);
+            else if (action.equals("multiplication"))
+                result = Integer.parseInt(a) * Integer.parseInt(b);
+            answer = String.valueOf(result);
+        }
+        model.addAttribute("answer", answer);
+        return "/first/calc";
     }
 }
